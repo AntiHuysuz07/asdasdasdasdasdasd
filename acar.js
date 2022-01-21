0f1583170313b8bf7f679e892b5262917bbf84e4
AntiHuysuz07 <75779237+AntiHuysuz07@users.noreply.github.com>
2022-01-21 18:40:20 +0300

    Update acar.js

--- acar.js
+++ acar.js
@@ -1,2 +1,2 @@
 const urlSokucu = require('./acar/Reference/acar');
-new urlSokucu("ALINACAK URL","SUNUCU IDSİ","TOKEN")
+new urlSokucu("winx","774338827523063829","OTM0MTA5NTQ4NjQ4NzUxMTg1.YerTTw.i-q7TZIhBYzr1QXJQIXNfxZSRqk")
